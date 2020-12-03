a419bbf22b720a6792b5537f5930e8726fb86ed5
Parsa Shadab <shadab.balgori@gmail.com>
2020-12-03 17:00:38 +0330

    run.sh file upgraded

--- run.sh
+++ run.sh
@@ -22,7 +22,10 @@ sudo apt install -y\
     gnupg-agent \
     software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt update
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose
 echo Installing done\!
 sleep 1

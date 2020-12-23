ed19aed30225acb528aab0783b1ce9aa9a136b58
Parsa Shadab <shadab.balgori@gmail.com>
2020-12-23 19:09:53 +0330

    Improvement applied to script functions

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#Laravel setup script for Debian-based distributions
+
+#Laravel setup script for Debian-based distributions (this will change later!)
 #Developed by PS-Professional
 #
 #
@@ -11,37 +12,89 @@
 #
 #===================================================================================================
 
-#Install Docker
+#Option for later release
+#SYS_PKG=`cat /etc/os-release | grep 'ID_LIKE' | cut -f 2 -d '='`
+
+#Functions
+function docker_install(){
+	#Install docker on your host
+	echo installing Docker\.\.\.
+	sleep 1
+	sudo apt update
+	sudo apt install -y\
+		apt-transport-https \
+		ca-certificates \
+		curl \
+		gnupg-agent \
+		software-properties-common
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+	sudo add-apt-repository \
+		"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+		$(lsb_release -cs) \
+		stable"
+	sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose
+	sleep 2
+	echo Installing done\!
+}
+
+function docker_update(){
+#Update your system
+	echo Updating system\.\.\.
+	sleep 1
+	sudo apt update && sudo apt upgrade
+	sleep 2
+	echo Updating Done\!
+}
+
+function docker_init(){
+	#config files
+	echo Gitting files from GitHub and running configurations\.\.\.
+	sleep 1
+	#git clone git https://github.com/laravel/laravel.git --branch=6.X laravel
+	sudo docker pull composer
+	sudo docker build -t ps/php:1.0 -t ps/php:latest .
+}
+
+#Main funcion
 echo Hello\!
 echo -e 'What you want me to do?\n1)Setup containers (init)\n2)Start containers (start)\n3)Stop containers (stop)'
 read -p '-> ' func
 case $func in
 	init )
-		echo installing Docker\.\.\.
+		if [[ -f /usr/bin/docker ]]
+		then
+			echo Docker is already exsits in your system
+			sleep 1
+			State=1
+			while [[ $State = 1 ]]
+			do
+				read -p 'Do you want to check for system updates? ' update
+				if [[ $update = 'yes' ]] || [[ $update = 'y' ]]
+				then
+					docker_update
+					State=0
+				elif [[ $update = 'no' ]] || [[ $update = 'n' ]]
+				then
+					echo OK\!
+					sleep 1
+					State=0
+				else
+					echo I didn\'t understand\!
+					sleep 0.5
+					echo Please try again\!
+					sleep 0.5
+				fi
+			done
+		else 
+			docker_install
+		fi
 		sleep 1
-		sudo apt update
-		sudo apt install -y\
-    			apt-transport-https \
-    			ca-certificates \
-    			curl \
-    			gnupg-agent \
-    			software-properties-common
-		curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-		sudo add-apt-repository \
-   		"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   			$(lsb_release -cs) \
-   			stable"
-		sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose
-		echo Installing done\!
-		sleep 1
-		#config files
-		echo Gitting files from GitHub and running configurations\.\.\.
-		sleep 1
-		#git clone git https://github.com/laravel/laravel.git --branch=6.X laravel
-		sudo docker pull composer
-		sudo docker build -t ps/php:1.0 -t ps/php:latest . ;;
+		docker_init ;;
 	start )
 		sudo docker-compose up -d;;
 	stop )
 		sudo docker-compose down;;
+
+	exit )
+		echo Goodbye\!;;
 esac

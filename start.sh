5cac3de97a4a9f8848f9d3377dcb9e8b06a60690
Parsa Shadab <shadab.balgori@gmail.com>
2021-01-17 13:11:06 +0330

    Fixed entrypoint file to keep container alive

--- start.sh
+++ start.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 /etc/init.d/apache2 start
+STATE=0
+while [[ $STATE -eq 0 ]]
+do
+APACHE_STATE=`/etc/init.d/apache2 status | grep 'Active' | sed  's/     //' | cut -f 2 -d ' '`
+if [[ $APACHE_STATE -eq active ]]
+then
+	echo 1 > /dev/null
+else
+	/etc/init.d/apache2 restart
+fi
+done

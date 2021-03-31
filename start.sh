93724649f52a6a6ea721640c882e4b35bebcffc4
Parsa Shadab <shadab.balgori@gmail.com>
2021-03-31 10:50:19 +0430

    Fixed Apache config problem

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 /etc/init.d/apache2 start
+cd /etc/apache2/sites-available && a2dissite 000-default.conf && a2ensite app.conf
+/etc/init.d/apache2 reload
+sleep 2
 STATE=0
 while [[ $STATE -eq 0 ]]
 do
